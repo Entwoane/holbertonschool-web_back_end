cf91515e0a51f3eef0abe3e441b37c8c39daf374
Entwoane <antoine.musci@gmail.com>
2025-03-31 19:08:38 +0200

    Add a blank line

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -2,6 +2,7 @@ export default function cleanSet(set, startString) {
   if (!startString) {
     return '';
   }
+
   return Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
