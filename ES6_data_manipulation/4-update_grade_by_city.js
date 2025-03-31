9a2d040c07eb0a8c133d6132031b6cefe9f7fa12
Entwoane <antoine.musci@gmail.com>
2025-03-31 19:02:14 +0200

    Initial commit

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(students, city, newGrade) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grade = newGrade.find((grade) => grade.studentId === student.id);
+
+      return {
+        ...student,
+        grade: grade ? grade.grade : 'N/A',
+      };
+    });
+}
