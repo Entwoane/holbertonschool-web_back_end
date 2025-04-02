19fe99c3e788851496f75c50c7d599d22c206281
Entwoane <antoine.musci@gmail.com>
2025-04-02 17:03:21 +0200

    dataview

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -4,8 +4,8 @@ export default function createInt8TypedArray(length, position, value) {
   }
 
   const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-  int8View[position] = value;
+  const int8View = new DataView(buffer);
+  int8View.setInt8(position, value);
 
   return buffer;
 }
